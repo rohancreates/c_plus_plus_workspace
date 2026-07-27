41beb70bf5a2e2d32d5993f8da87237991acf0cd
rohancreates <rohankumarcreates@gmail.com>
2026-07-27 22:59:29 +0530

    stack through linklist

--- sem3/Adv DSA/stack_linklist.cpp
+++ sem3/Adv DSA/stack_linklist.cpp
@@ -1,68 +1,105 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-Struct Node{
+
+struct Node {
     int info;
     Node* next;
-}
-Node* top = NULL;
-Void push(int x){
-    Node* temp = new Node;
-    temp-> info = x;
-    temp->next= top;
-    top=temp;
-}
-bool isunderflow(){
-    if(top==NULL){
-        cout<<"stack is empty"<<endl;
-        return ;
-    }
-}
-Void pop(){
-    if(isunderflow){}
-    else{
-        Node* temp = top;
-        top=top->next;
-        delete temp;
-    }
-}
-Void peek(){
-    if(isunderflow){}
-        else{
-            cout<<top->info;
-        }
-}
-Void traverse(){
-    if(isunderflow){}
-        else{
-            Node* temp = top;
-            while(temp!=NULL){
-                cout<<temp->info<<" ";
-                temp=temp->next;
-            }
-        }
-}
-int main(){
-    Stack s1;
-    int ch,value;
-    do{
-        cout<<"1.push";
-        cout<<"2.pop";
-        cout<<"3.peek";
-        cout<<"4.exit";
-        cin>>ch;
-        switch(ch):{
-        case 1 : cout<<"enter elem";
-            cin>>value;
-            s.push(value);
-            break;
-        case 2 : s1.pop();
-            break;
-        case 3 : s1.peek();
-            break;
-        case 4 : s1.traverse();
-            break;
-    default : cout<<"invalid choice";
+};
 
-        }while(){}
+Node* top = NULL;
+
+// Push
+void push(int x) {
+    Node* temp = new Node;
+    temp->info = x;
+    temp->next = top;
+    top = temp;
+}
+
+// Check Underflow
+bool isUnderflow() {
+    if (top == NULL) {
+        cout << "Stack is Empty!" << endl;
+        return true;
     }
+    return false;
+}
+
+// Pop
+void pop() {
+    if (isUnderflow())
+        return;
+
+    Node* temp = top;
+    top = top->next;
+    cout << "Deleted: " << temp->info << endl;
+    delete temp;
+}
+
+// Peek
+void peek() {
+    if (isUnderflow())
+        return;
+
+    cout << "Top Element: " << top->info << endl;
+}
+
+// Traverse
+void traverse() {
+    if (isUnderflow())
+        return;
+
+    Node* temp = top;
+
+    cout << "Stack Elements: ";
+    while (temp != NULL) {
+        cout << temp->info << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int ch, value;
+
+    do {
+        cout << "\n----- STACK MENU -----\n";
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Traverse\n";
+        cout << "5. Exit\n";
+        cout << "Enter Choice: ";
+        cin >> ch;
+
+        switch (ch) {
+            case 1:
+                cout << "Enter Element: ";
+                cin >> value;
+                push(value);
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                peek();
+                break;
+
+            case 4:
+                traverse();
+                break;
+
+            case 5:
+                cout << "Exiting...\n";
+                break;
+
+            default:
+                cout << "Invalid Choice!\n";
+        }
+
+    } while (ch != 5);
+
+    return 0;
 }
